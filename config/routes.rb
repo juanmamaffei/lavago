1199960b59a02ec9d18df38f31dd5adf51ada3e8
juanmamaffei <juanmamaffei@bitbucket.com>
2019-01-26 17:05:12 -0300

    Componente de login y registration con REACT

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,19 @@ Rails.application.routes.draw do
   resources :orders do
     resources :deliveries
   end
-  get 'home/index'
+  
+  # get 'home/index'
 
-  devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, :controllers => {
+    omniauth_callbacks: "users/omniauth_callbacks",
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  
+
+  authenticated :user do
+    root "home#index"
+  end
+  root "home#login"
 end
